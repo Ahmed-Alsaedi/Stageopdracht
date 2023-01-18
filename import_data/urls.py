cdb6e557eab5a13bb41cc702fa4a541754882b29
Ahmed Alsaedi <ahmed_alsaedi@live.nl>
2023-01-18 22:33:42 +0100

    city select view en city select html aangepast.

--- import_data/urls.py
+++ import_data/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import city_select_view
+from . import views
+from django.contrib import admin
 
 urlpatterns = [
-    path('city-select/', city_select_view, name='city_select'),
+    path('', views.select_city),
+    path('get_hotels/', views.get_hotels, name='get_hotels'),
+    path('update/', views.update, name='run_command'),
 ]
