fc019cc21c14a0504797989dcb82ec1387c432fa
Ahmed Alsaedi <ahmed_alsaedi@live.nl>
2023-01-20 03:26:06 +0100

    Admin toegevoegd

--- import_data/management/commands/Command.py
+++ import_data/management/commands/Command.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     
     def handle(self,*args, **options):
         """Handle method needs to be called handle, otherwise Command doesn't work.
-        Uses requests libary to authenticate itself and pass the responses to the corresponding
+        Users requests libary to authenticate itself and pass the responses to the corresponding
         methods"""
         
         city_url = "http://rachel.maykinmedia.nl/djangocase/city.csv"
--- import_data/urls.py
+++ import_data/urls.py
@@ -7,3 +7,8 @@ urlpatterns = [
     path('get_hotels/', views.get_hotels, name='get_hotels'),
     path('update/', views.update, name='run_command'),
 ]
+
+# headers and site title for the admin portal
+admin.site.site_header = "Admin portal"
+admin.site.site_title = "Admin portal"
+admin.site.index_title = "Welcome to the admin portal"
